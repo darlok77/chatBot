014ebc96b87f60b1383de399e60175ce0c88f34a
darlok77 <g.dalleau77@hotmail.com>
2018-02-02 14:17:34 +0100

    add class and bowerify

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-const express =require('express'),
-    app= express();
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-    fs = require('fs'),
-    path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const path = require('path');
 
-
-
-app.use('/', express.static(path.join(__dirname+'/public')));
+app.use ('/', express.static(path.join(`${__dirname}/public`)));
 
 // Chargement de la page index.html
-app.get('/', (req, res) => {
+app.get ('/', (req, res) => {
   res.sendfile('./index.html');
 });
 
-io.sockets.on('connection',  (socket, pseudo) => {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', (pseudo) => {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
-    });
+io.sockets.on('connection', socket => {
+  // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+  socket.on('nouveau_client', pseudo => {
+    socket.pseudo = pseudo;
+    socket.broadcast.emit('nouveau_client', pseudo);
+  });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', (message) => {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    }); 
+  // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+  socket.on('message', message => {
+    socket.broadcast.emit('message', {'pseudo': socket.pseudo, 'message': message});
+  });
 });
 
 server.listen(8080);
--- public/index.js
+++ public/index.js
@@ -1,53 +1,67 @@
-  const el_zone_chat = document.querySelector("#zone_chat");
-  const el_message = document.querySelector("#message");
-  const el_newP = document.createElement("p");
-  const el_newB = document.createElement("b");
+class Chat {
+  constructor () {
 
-  // Connexion à socket.io
-  const socket = io.connect('http://localhost:8080');
-
-  // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
-  const pseudo = prompt('Quel est votre pseudo ?');
-  socket.emit('nouveau_client', pseudo);
-  document.title = pseudo + ' - ' + document.title;
-
-  // Quand on reçoit un message, on l'insère dans la page
-  socket.on('message',(data) => {
-    insereMessage(data.pseudo, data.message)
-  })
-
-  // Quand un nouveau client se connecte, on affiche l'information
-  socket.on('nouveau_client', (pseudo)=> {
-
-    const content = document.createTextNode(pseudo + ' a rejoint le Chat !\n');
-    el_zone_chat.appendChild(el_newP);
-    el_newP.appendChild(content);
-    //document.body.insert(el_newP,el_zone_chat);
-  })
-
-
-            
-el_message.addEventListener("keypress",(e) => {
-
-  if(e.keyCode==13){
-    let message = el_message.value;
-    socket.emit('message', message); // Transmet le msg aux autres
-    insereMessage(pseudo, message); // Affiche le msg aussi sur notre page
-    el_message.value='';  // Vide la zone
-    el_message.value.focus();  // remet le focus dessus
   }
-});
 
+  init () {
 
-function insereMessage(pseudo, message) {
-                 
-  const msgContent = document.createTextNode(message +'\n');
-  const msgPseudo = document.createTextNode(pseudo+' : ');
+  }
 
-  el_zone_chat.appendChild(el_newP);
-  el_newP.appendChild(el_newB);
-  el_newB.appendChild(msgPseudo);
-  el_newP.appendChild(msgContent);
+  run () {
+    const elZoneChat = document.querySelector('#zone_chat');
+    const elMessage = document.querySelector('#message');
 
-  document.body.insert(el_newP,el_zone_chat);
+    // Connexion à socket.io
+    const io = require('socket.io-client');
+    const socket = io.connect('http://localhost:8080');
+
+    // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
+    const pseudo = prompt('Quel est votre pseudo ?');
+
+    socket.emit('nouveau_client', pseudo);
+    document.title = pseudo + ' - ' + document.title;
+
+    // Quand on reçoit un message, on l'insère dans la page
+    socket.on('message', (data) => {
+      insereMessage(data.pseudo, data.message);
+    });
+
+    // Quand un nouveau client se connecte, on affiche l'information
+    socket.on('nouveau_client', (pseudo)=> {
+      const content = document.createTextNode(pseudo + ' a rejoint le Chat !');
+      const elNewP = document.createElement('div');
+
+      elZoneChat.appendChild(elNewP);
+      elNewP.appendChild(content);
+      //document.body.insert(elNewP, elZoneChat);
+    });
+
+    elMessage.addEventListener ('keypress', (e) => {
+      if (e.keyCode === 13) {
+        let message = elMessage.value;
+
+        socket.emit('message', message); // Transmet le msg aux autres
+        insereMessage(pseudo, message); // Affiche le msg aussi sur notre page
+        elMessage.value = ''; // Vide la zone
+        elMessage.value.focus(); // remet le focus dessus
+      }
+    });
+
+    function insereMessage (pseudo, message) {
+      const msgContent = document.createTextNode (message);
+      const msgPseudo = document.createTextNode(pseudo + ' : ');
+      const elNewP = document.createElement('p');
+      const elNewB = document.createElement('b');
+
+      elZoneChat.appendChild(elNewP);
+      elNewP.appendChild(elNewB);
+      elNewB.appendChild(msgPseudo);
+      elNewP.appendChild(msgContent);
+      document.body.insert(elNewP, elZoneChat);
+    }
+  }
 }
+
+const chat = new Chat();
+
+chat.run();
