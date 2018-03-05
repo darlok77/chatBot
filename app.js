97abebafe6c267c3e975347f82b9ce73f63578a7
darlok77 <g.dalleau77@hotmail.com>
2018-03-05 23:14:28 +0100

    add a bot Carrefour and all function

--- app.js
+++ app.js
@@ -24,24 +24,61 @@ io.sockets.on('connection', socket => {
     socket.broadcast.emit('message', {'pseudo': socket.pseudo, 'message': message});
   });
 
-  
   socket.on('messageytb', message => {
-  const tabVideos=[];
+    const tabVideos = [];
 
-  request({url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${message}&key=AIzaSyBzhXQGlpp20V71dGCT_67REdUlWe-Gpog`, json: true}, function(err, res, json) {
-    if (err) {
-      throw err;
-    }
+    request({'url': `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${message}&key=AIzaSyBzhXQGlpp20V71dGCT_67REdUlWe-Gpog`, 'json': true}, (err, res, json)=> {
+      if (err) {
+        throw err;
+      }
+
+      for (let i = 0; i < 3; i ++) {
+        tabVideos.push({'id': json.items[i].id.videoId, 'title': json.items[i].snippet.title, 'channelTitle': json.items[i].snippet.channelTitle, 'thumbnails': json.items[i].snippet.thumbnails.default.url});
+      }
+      socket.emit('messageytb', {'pseudo': socket.pseudo, 'message': tabVideos});
+    });
+  });
+
+  socket.on('messageCarrefour', message => {
     
-    for (let i = 0; i < 3; i++){
-      tabVideos.push({'id': json.items[i].id.videoId , 'title': json.items[i].snippet.title , 'channelTitle': json.items[i].snippet.channelTitle, 'thumbnails': json.items[i].snippet.thumbnails.default.url});
-    }
-    socket.emit('messageytb', {'pseudo': socket.pseudo, 'message': tabVideos});
 
+    request({url: 'https://api.fr.carrefour.io/v1/openapi/stores', json: true}, function(err, res, json) {
+      if (err) {
+        throw err;
+      } else {
+        const appCredentials = {
+          "id_client" : "98cc4890-9c5b-4a9c-b2d9-3c9fdf7c4c18",
+          "secret"    : "C1yD0eY7sT2yL8sJ4yR4tX5fW7eP7tV1dC6qA7fX4aU1gQ8oX8"
+        }
+
+        //var request = require("request");
+        const options = { method: 'GET',
+          url: 'https://api.fr.carrefour.io/v1/openapi/stores',
+          qs: { 
+            longitude: message[1],
+            latitude: message[0],
+            radius: '15000',
+          },
+          headers: { 
+            accept: 'application/json',
+            'x-ibm-client-secret': appCredentials.secret,
+            'x-ibm-client-id':  appCredentials.id_client
+          } 
+        };
+
+        request(options, function (error, response, body) {
+          if (error) return  socket.emit('messageCarrefour', 'error');
+        
+          else {
+            const json = JSON.parse(body);
+            for(var i=0; i < json.list.length; i++){
+              socket.emit('messageCarrefour', {'addresse':json.list[i].address, 'banner':json.list[i].banner, 'city':json.list[i].city, 'gas_station':json.list[i].gas_station, 'drive':json.list[i].drive,'latitude':json.list[i].latitude, 'longitude':json.list[i].longitude});
+            }   
+          }
+        });
+      }
     });
   });
 });
-
-
 
 server.listen(8080);
--- public/index.js
+++ public/index.js
@@ -11,45 +11,108 @@ class Chat {
     const elZoneChat = document.querySelector('#zone_chat');
     const elMessage = document.querySelector('#message');
 
-    // Connexion à socket.io
+    // Connexion socket.io
     const io = require('socket.io-client');
-    const socket = io.connect('http://localhost:8080')
+    const socket = io.connect('http://localhost:8080');
 
-    // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
+    // ask a pseudo for a new connection and send in a title
     const pseudo = prompt('Quel est votre pseudo ?');
 
+    //send new user pseudo
     socket.emit('nouveau_client', pseudo);
-    document.title = pseudo + ' - ' + document.title;
+    document.title = `${pseudo} - ${document.title}`;
 
-    // Quand on reçoit un message, on l'insère dans la page
-    socket.on('message', (data) => {
+    // receive a normal message
+    socket.on('message', data => {
       insereMessage(data.pseudo, data.message);
     });
 
-    // Quand on reçoit un message avec /youtube, on l'insère dans la page
-   /* socket.on('messageytb', (data) => {
-      
-      insereVideo(data.message[0].id);
-    });*/
 
-    socket.on('messageytb', (data) => {
+    socket.on('messageCarrefour', data => {
+      selectionCarrefour(data); 
+    });
 
-      let DOM = "";
+    socket.on('messageytb', data => {
+      selectionVideo(data);
+    });
+
+    // send a new user log
+    socket.on('nouveau_client', pseudo=> {
+      const content = document.createTextNode(`${pseudo} a rejoint le Chat !`);
       const elNewDiv = document.createElement('div');
 
+      elZoneChat.appendChild(elNewDiv);
+      elNewDiv.appendChild(content);
+      elZoneChat.scrollTop = elZoneChat.scrollHeight;
+    });
 
-      data.message.forEach(function(element) {
+    elMessage.addEventListener ('keypress', e => {
+      if (e.keyCode === 13) {
+        let message = elMessage.value;
 
+        if (message.indexOf('/youtube') != - 1) {
+
+          message = message.substring(message.indexOf('/youtube') + 9);
+          socket.emit('messageytb', message);
+
+        }else if (message.indexOf('/carrefour') != - 1){
+
+
+          function getLocation() {
+              if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(showPosition);
+              }
+          }
+
+          function showPosition(position) {
+
+            const positionGPS=[position.coords.latitude,position.coords.longitude];
+
+            socket.emit('messageCarrefour', positionGPS);
+
+
+          }
+          getLocation();
+
+
+        } else {
+          socket.emit('message', message); // send message in other user
+        }
+        insereMessage(pseudo, message); // display a msg in our page
+        elMessage.value = ''; // reset an area
+        elMessage.focus(); // focus on a message area
+      }
+    });
+
+    const insereMessage = (pseudo, message) => {
+      const msgContent = document.createTextNode (message);
+      const msgPseudo = document.createTextNode(`${pseudo}  : `);
+      const elNewDiv = document.createElement('div');
+      const elNewB = document.createElement('b');
+
+      elZoneChat.appendChild(elNewDiv);
+      elNewDiv.appendChild(elNewB);
+      elNewB.appendChild(msgPseudo);
+      elNewDiv.appendChild(msgContent);
+      elZoneChat.scrollTop = elZoneChat.scrollHeight;
+    };
+
+    const selectionVideo= data => {
+
+      const elNewDiv = document.createElement('div');
+
+      data.message.forEach(element => {
         const elNewDiv2 = document.createElement('div');
-          elNewDiv2.setAttribute("class", "descVideo");
+
+        elNewDiv2.setAttribute('class', 'descVideo');
 
         const elNewImg = document.createElement('img');
-        elNewImg.setAttribute("src", element.thumbnails);
-        elNewImg.setAttribute("id", element.id);
-        elNewImg.setAttribute("class", "thumbnails");
-        const elNewP= document.createElement('p');
-        const elNewSpan = document.createElement('span');
 
+        elNewImg.setAttribute('src', element.thumbnails);
+        elNewImg.setAttribute('id', element.id);
+        elNewImg.setAttribute('class', 'thumbnails');
+        const elNewP = document.createElement('p');
+        const elNewSpan = document.createElement('span');
 
         const title = document.createTextNode (element.title);
         const chanel = document.createTextNode (element.channelTitle);
@@ -60,69 +123,92 @@ class Chat {
         elNewDiv2.appendChild(elNewP);
         elNewDiv2.appendChild(elNewSpan);
 
-
-      elNewImg.addEventListener('click', (e) => {
-        insereVideo(element.id);
-      });
+        elNewImg.addEventListener('click', e => {
+          insereVideo(element.id);
+        });
 
         elNewDiv.appendChild(elNewDiv2);
       });
       elZoneChat.appendChild(elNewDiv);
-    });
-
-    // Quand un nouveau client se connecte, on affiche l'information
-    socket.on('nouveau_client', (pseudo)=> {
-      const content = document.createTextNode(pseudo + ' a rejoint le Chat !');
-      const elNewDiv = document.createElement('div');
-
-      elZoneChat.appendChild(elNewDiv);
-      elNewDiv.appendChild(content);
-      elZoneChat.scrollTop = elZoneChat.scrollHeight;
-    });
-
-    elMessage.addEventListener ('keypress', (e) => {
-      if (e.keyCode === 13) {
-        let message = elMessage.value;
-
-        if (message.indexOf("/youtube")!= -1){
-            message = message.substring(message.indexOf("/youtube")+9)
-          socket.emit('messageytb', message);
-        }
-        else{
-          socket.emit('message', message); // Transmet le msg aux autres
-        }
-        insereMessage(pseudo, message); // Affiche le msg aussi sur notre page
-        elMessage.value = ''; // Vide la zone
-        elMessage.focus(); // remet le focus dessus
-      }
-    });
-
-    function insereMessage (pseudo, message) {
-      const msgContent = document.createTextNode (message);
-      const msgPseudo = document.createTextNode(pseudo + ' : ');
-      const elNewDiv = document.createElement('div');
-      const elNewB = document.createElement('b');
-
-      elZoneChat.appendChild(elNewDiv);
-      elNewDiv.appendChild(elNewB);
-      elNewB.appendChild(msgPseudo);
-      elNewDiv.appendChild(msgContent);
-      elZoneChat.scrollTop = elZoneChat.scrollHeight;
     }
 
-    function insereVideo (id) {
 
-       //<div id="player"></div>
+    const insereVideo = id => {
       const elNewIframe = document.createElement('iframe');
-      elNewIframe.setAttribute("id", "player");
-      elNewIframe.setAttribute("type", "text/html");
-      elNewIframe.setAttribute("width", "640");
-      elNewIframe.setAttribute("height", "360");
-      elNewIframe.setAttribute("src", `http://www.youtube.com/embed/${id}`);
+
+      elNewIframe.setAttribute('id', 'player');
+      elNewIframe.setAttribute('type', 'text/html');
+      elNewIframe.setAttribute('width', '640');
+      elNewIframe.setAttribute('height', '360');
+      elNewIframe.setAttribute('src', `http://www.youtube.com/embed/${id}`);
 
       elZoneChat.appendChild(elNewIframe);
+    };
+
+    const selectionCarrefour= data => {
+      alert(JSON.stringify(data));
+
+      const elNewDiv = document.createElement('div');
+
+      data.forEach(element => {   ///  ici
+        const elNewDiv2 = document.createElement('div');
+
+        elNewDiv2.setAttribute('class', 'descCarrefour');
+
+        const elNewPAdresse = document.createElement('p');
+        const elNewPBanner = document.createElement('p');
+        const elNewPService = document.createElement('p');
+
+        const elNewSpan = document.createElement('span');
+
+        const adresse = document.createTextNode (element.addresse);
+        const city = document.createTextNode (element.city);
+
+        const banner = document.createTextNode (element.banner);
+
+        const gas_station = document.createTextNode ('No gas station');
+        if(element.gas_station =! 0) gas_station =document.createTextNode ('gas station');
+       
+        const drive = document.createTextNode ('No drive');
+        if(element.drive != 0) drive =document.createTextNode ('drive');
+
+
+        elNewPAdresse.appendChild(adresse);
+        elNewPAdresse.appendChild(city);
+        elNewPBanner.appendChild(banner);
+        elNewPService.appendChild(gas_station);
+        elNewPService.appendChild(drive);
+
+        elNewDiv2.appendChild(elNewPAdresse);
+        elNewDiv2.appendChild(elNewPBanner);
+        elNewDiv2.appendChild(elNewPService);
+
+        elNewDiv2.addEventListener('click', e => {
+          insereCarrefourMap(element.latitude,element.longitude);
+        });
+
+        elNewDiv.appendChild(elNewDiv2);
+      });
+      elZoneChat.appendChild(elNewDiv);
+
     }
 
+    const insereCarrefourMap = (latitude,longitude) => {
+      alert(latitude);
+      alert(longitude);
+
+      const elNewIframe = document.createElement('iframe');
+
+      elNewIframe.setAttribute('width', '600');
+      elNewIframe.setAttribute('height', '450');
+      elNewIframe.setAttribute('frameborder', '0');
+      elNewIframe.setAttribute('style', 'border:0');
+      elNewIframe.setAttribute('src', `https://www.google.com/maps/embed/v1/place?key=AIzaSyBzhXQGlpp20V71dGCT_67REdUlWe-Gpog&q=${latitude},${longitude}`);
+      //elNewIframe.setAttribute('src', `https://www.google.com/maps/embed/v1/place?key=AIzaSyBzhXQGlpp20V71dGCT_67REdUlWe-Gpog&origine=${},${}&destination=${latitude},${longitude}`);
+      //elNewIframe.setAttribute('allowfullscreen');
+
+      elZoneChat.appendChild(elNewIframe);
+    };
   }
 }
 
